296f13123d346363c08bec387575c4213bbea5f6
nonsodaniel <nonsodaniel07@gmail.com>
2019-10-08 15:39:15 +0100

    Added syntetic voices list

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-const synth = window.SpeechSynthesis;
+const synth = window.speechSynthesis;
 
 //DOM elements
 const textForm = document.querySelector("form");
@@ -7,5 +7,32 @@ const voiceSelect = document.querySelector("#voice-select");
 const rateValue = document.querySelector("#rate-value");
 const pitch = document.querySelector("#pitch");
 const pitchValue = document.querySelector("#pitch-value");
+
+console.log(synth)
+//init voices array
+let voices = [];
+
+const getVoices = () => {
+    voices = synth.getVoices();
+    console.log(voices)
+
+    //loop through voices  and create option for each
+    voices.forEach((voice) => {
+        //create option element
+        const option = document.createElement("option");
+        //fill option with voice and language
+        option.textContent = `${voice.name} ( ${voice.lang} )`;
+
+        //set needed option attributes 
+        option.setAttribute("data-lang", voice.lang)
+        option.setAttribute("data-name", voice.name)
+        voiceSelect.appendChild(option);
+    })
+}
+getVoices()
+//in firefox , this doesn't exist
+if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+}
 
 
