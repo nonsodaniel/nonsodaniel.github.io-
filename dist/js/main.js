3a8507130caa9f4a64819c2f359078fbc144915f
nonsodaniel <nonsodaniel07@gmail.com>
2019-10-08 15:07:56 +0100

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+const synth = window.SpeechSynthesis;
+
+//DOM elements
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+const rateValue = document.querySelector("#rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
+
+
